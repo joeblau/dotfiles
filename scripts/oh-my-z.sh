4a2be539fb5629469647b9530321b6e4f996c1e6
Joe Blau <josephblau@gmail.com>
2017-08-05 13:02:00 -0400

    Added support for nvm

--- scripts/oh-my-z.sh
+++ scripts/oh-my-z.sh
@@ -5,3 +5,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # Install Power Level 9000 Theme
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Install NVM
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
