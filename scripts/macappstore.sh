8d97fcf0242657416c01b88f5f7ba310f57a1bf5
Joseph Blau <josephblau@Josephs-MBP.fios-router.home>
2017-09-06 20:39:10 -0400

    Updated mac app store

--- scripts/macappstore.sh
+++ scripts/macappstore.sh
@@ -8,12 +8,8 @@ mas install 603117688
 mas install 1081413713
 # Color Picker
 mas install 641027709
-# Realm Browser
-mas install 1007457278
 # Xcode
 mas install 497799835
-# Pixelmator
-mas install 407963104
 # Deckset
 mas install 847496013
 # Airmail 3
@@ -22,29 +18,17 @@ mas install 918858936
 mas install 537211143
 # Unclutter
 mas install 577085396
-# Flume
-mas install 792425898
 # Tweetbot
 mas install 557168941
-# Claquette
-mas install 587748131
 # Deliveries
 mas install 924726344
-# Svgsus
-mas install 1106867065
-# GIPHY CAPTURE
-mas install 668208984
 # Spectrum
 mas install 518156125
-# GIFMaker
-mas install 1015930892
 # Fantastical 2
 mas install 975937182
-# Core Animator
-mas install 934434650
-# Caffeine
-mas install 411246225
-# Skitch
-mas install 425955336
 # 1Password
 mas install 443987910
+# Affinity Photo
+mas install 824183456 
+# Affinity Designer
+mas install 824171161
