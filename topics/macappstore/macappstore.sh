8c70c8fa4e22ecc7e3bf656a9d962ddcac2161f2
Joe Blau <im@joeblau.com>
2020-01-03 18:43:51 -0800

    Update mac app store apps

--- topics/macappstore/macappstore.sh
+++ topics/macappstore/macappstore.sh
@@ -4,8 +4,6 @@
 
 # GIF Brewery 3
 mas install 1081413713
-# Color Picker
-mas install 641027709
 # Deckset
 mas install 847496013
 # Tweetbot 3
@@ -22,12 +20,12 @@ mas install 443987910
 mas install 824183456 
 # Affinity Designer
 mas install 824171161
+# Affinity Publisher
+mas install 881418622
 # Pastebot
 mas install 1179623856
 # StopTheMadness
 mas install 1376402589
-# Haskell Development Platfrom
-mas install 841285201
 # Magnet
 mas install 441258766
 # Slack
@@ -38,3 +36,7 @@ mas install 924726344
 mas install 1107421413
 # Skitch
 mas install 425955336
+# Spark
+mas install 1176895641
+# iA Writer
+mas install 775737172
